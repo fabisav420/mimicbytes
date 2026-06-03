1ade2d47cf7ea81314f73d905f103c77e390dcef
amhundt <44945519+amhundt@users.noreply.github.com>
2026-06-03 08:07:24 +0200

    Add files via upload
    Aktualisiertes Websitedesign und -Layout

--- script.js
+++ script.js
@@ -160,3 +160,24 @@ carousels.forEach(carousel => {
   const yearEl = document.getElementById("copyrightYear");
   if (yearEl) yearEl.textContent = new Date().getFullYear();
 });
+
+
+
+
+
+
+
+// === bewegende Augen im Logo ===
+document.querySelector("body").addEventListener("mousemove", eyeball);
+
+function eyeball() {
+  let eyes = document.querySelectorAll(".eye");
+  eyes.forEach((eye) => {
+    let x = eye.getBoundingClientRect().left + eye.clientWidth / 2;
+    let y = eye.getBoundingClientRect().top + eye.clientHeight / 2;
+
+    let radian = Math.atan2(event.pageX - x, event.pageY - y);
+    let rotate = radian * (180 / Math.PI) * -1 + 270;
+    eye.style.transform = `rotate(${rotate}deg)`;
+  });
+}
